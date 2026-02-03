cc1838f0e41b9d50b4764163349c07fcffb8d241
nexora-academy <kareem.abdelmuttalib.soc@gmail.com>
2026-02-03 18:39:31 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 (function ($) {
     "use strict";
 
-    // 1. Spinner - تم تعديله ليضمن الاختفاء عند الترجمة
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1000); // زدنا الوقت قليلاً لضمان استقرار الصفحة
+    // 1. إدارة الـ Spinner
+    var hideSpinner = function () {
+        if ($('#spinner').length > 0) {
+            $('#spinner').removeClass('show');
+        }
     };
-    spinner();
-    
-    // 2. دوال الترجمة (Google Translate)
-    // دالة التهيئة
+    // إخفاء إجباري بعد 3 ثواني حتى لو فشل كل شيء
+    setTimeout(hideSpinner, 3000);
+
+    // 2. إعدادات جوجل للترجمة (Global)
     window.googleTranslateElementInit = function() {
         new google.translate.TranslateElement({
             pageLanguage: 'en',
@@ -21,7 +19,7 @@
         }, 'google_translate_element');
     };
 
-    // دالة تغيير اللغة وحفظها
+    // 3. دالة تغيير اللغة وحفظها
     window.changeLanguage = function(lang) {
         localStorage.setItem('userLanguage', lang);
         var selectField = document.querySelector(".goog-te-combo");
@@ -31,26 +29,23 @@
         }
     };
 
-    // مراقبة تحميل الصفحة لتطبيق اللغة المحفوظة تلقائياً
-    $(window).on('load', function () {
-        var checkTranslate = setInterval(function() {
-            var selectField = document.querySelector(".goog-te-combo");
-            var savedLang = localStorage.getItem('userLanguage');
-            if (selectField) {
-                if (savedLang && savedLang !== 'en') {
-                    changeLanguage(savedLang);
-                }
-                clearInterval(checkTranslate);
+    // 4. التشغيل الذكي (يراقب ظهور محرك جوجل باستمرار)
+    var checkTranslateReady = setInterval(function() {
+        var selectField = document.querySelector(".goog-te-combo");
+        var savedLang = localStorage.getItem('userLanguage');
+        
+        if (selectField) {
+            hideSpinner(); // اخفي التحميل فوراً عند جاهزية المترجم
+            if (savedLang && savedLang !== 'en') {
+                window.changeLanguage(savedLang);
             }
-        }, 500);
-    });
+            clearInterval(checkTranslateReady);
+        }
+    }, 500);
 
-    // --- بقية أكواد القالب الأصلي ---
-
-    // Initiate the wowjs
+    // --- أكواد القالب الباقية (WOW, Scroll, etc.) ---
     new WOW().init();
 
-    // Sticky Navbar
     $(window).scroll(function () {
         if ($(this).scrollTop() > 45) {
             $('.nav-bar').addClass('sticky-top shadow-sm');
@@ -59,37 +54,12 @@
         }
     });
 
-    // Testimonial-carousel
     $(".testimonial-carousel").owlCarousel({
-        autoplay: true,
-        smartSpeed: 2000,
-        center: false,
-        dots: false,
-        loop: true,
-        margin: 25,
-        nav : true,
-        navText : [
-            '<i class="bi bi-arrow-left"></i>',
-            '<i class="bi bi-arrow-right"></i>'
-        ],
-        responsiveClass: true,
-        responsive: {
-            0:{items:1},
-            576:{items:1},
-            768:{items:2},
-            992:{items:2},
-            1200:{items:2}
-        }
+        autoplay: true, smartSpeed: 2000, center: false, dots: false, loop: true, margin: 25, nav : true,
+        navText : ['<i class="bi bi-arrow-left"></i>','<i class="bi bi-arrow-right"></i>'],
+        responsive: { 0:{items:1}, 768:{items:2}, 1200:{items:2} }
     });
 
-   // Back to top button
-   $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
