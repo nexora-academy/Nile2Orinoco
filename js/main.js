719c3333cbce553b9debba4a2cc298219d5be07c
nexora-academy <kareem.abdelmuttalib.soc@gmail.com>
2026-02-03 18:33:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,54 @@
 (function ($) {
     "use strict";
 
-    // Spinner
+    // 1. Spinner - تم تعديله ليضمن الاختفاء عند الترجمة
     var spinner = function () {
         setTimeout(function () {
             if ($('#spinner').length > 0) {
                 $('#spinner').removeClass('show');
             }
-        }, 1);
+        }, 1000); // زدنا الوقت قليلاً لضمان استقرار الصفحة
     };
-    spinner(0);
+    spinner();
     
-    
+    // 2. دوال الترجمة (Google Translate)
+    // دالة التهيئة
+    window.googleTranslateElementInit = function() {
+        new google.translate.TranslateElement({
+            pageLanguage: 'en',
+            includedLanguages: 'ar,en,es',
+            autoDisplay: false
+        }, 'google_translate_element');
+    };
+
+    // دالة تغيير اللغة وحفظها
+    window.changeLanguage = function(lang) {
+        localStorage.setItem('userLanguage', lang);
+        var selectField = document.querySelector(".goog-te-combo");
+        if (selectField) {
+            selectField.value = lang;
+            selectField.dispatchEvent(new Event('change'));
+        }
+    };
+
+    // مراقبة تحميل الصفحة لتطبيق اللغة المحفوظة تلقائياً
+    $(window).on('load', function () {
+        var checkTranslate = setInterval(function() {
+            var selectField = document.querySelector(".goog-te-combo");
+            var savedLang = localStorage.getItem('userLanguage');
+            if (selectField) {
+                if (savedLang && savedLang !== 'en') {
+                    changeLanguage(savedLang);
+                }
+                clearInterval(checkTranslate);
+            }
+        }, 500);
+    });
+
+    // --- بقية أكواد القالب الأصلي ---
+
     // Initiate the wowjs
     new WOW().init();
-
 
     // Sticky Navbar
     $(window).scroll(function () {
@@ -24,7 +58,6 @@
             $('.nav-bar').removeClass('sticky-top shadow-sm');
         }
     });
-
 
     // Testimonial-carousel
     $(".testimonial-carousel").owlCarousel({
@@ -41,32 +74,21 @@
         ],
         responsiveClass: true,
         responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:2
-            },
-            1200:{
-                items:2
-            }
+            0:{items:1},
+            576:{items:1},
+            768:{items:2},
+            992:{items:2},
+            1200:{items:2}
         }
     });
 
-    
    // Back to top button
    $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
@@ -74,4 +96,3 @@
     });
 
 })(jQuery);
-
